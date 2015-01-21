42a67323d6d912f182b55338a07a812f9f5382d2
japple1 <jsolis_11@hotmail.com>
2015-01-20 21:49:33 -0300

    Se agregaron variables a Entity

--- src/com/makzk/games/entities/Entity.java
+++ src/com/makzk/games/entities/Entity.java
@@ -1,11 +1,17 @@
 package com.makzk.games.entities;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 
 public class Entity {
 	protected GameContainer gc;
 	protected float speedX;
 	protected float speedY;
+	protected Color color;
+	protected boolean keepOnScreen = true;
+	protected boolean gravity = false;
+	protected boolean solid = true;
+	protected boolean onGround = false;
 
 	public Entity(GameContainer gc) {
 		this.gc = gc;
@@ -28,5 +34,16 @@ public class Entity {
 	public void setSpeedY(float speedY) {
 		this.speedY = speedY;
 	}
+	
+	public Color getColor() {
+		return color;
+	}
 
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	public boolean isOnGround() {
+		return onGround;
+	}
 }
