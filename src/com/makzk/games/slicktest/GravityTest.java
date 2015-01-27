fa72484ad013a7f634bf0dc2dd73c4843678014b
japple1 <jsolis_11@hotmail.com>
2015-01-27 00:36:11 -0300

    Se creo la carpeta data junto con la carpeta sprite

--- src/com/makzk/games/slicktest/GravityTest.java
+++ src/com/makzk/games/slicktest/GravityTest.java
@@ -34,7 +34,7 @@ public class GravityTest extends BasicGame {
 		
 		Rectangle r = new Rectangle(10, 201, 20, 50);
 		player = new Player(gc, r);
-		player.setColor(Color.blue);
+		player.setColor(Color.transparent);
 	}
 
 	@Override
