b7dec1b5f6db3e012c6fac991a5ed26f05d8a9b1
makzk <me@makzk.com>
2015-03-04 16:22:32 -0300

    Se ordenan los botones en el menu La accion a ejecutar se guarda en el boton.

--- src/com/makzk/games/states/Menu.java
+++ src/com/makzk/games/states/Menu.java
@@ -9,46 +9,35 @@ import com.makzk.games.Main;
 import com.makzk.games.elements.Button;
 import com.makzk.games.util.ImageManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu extends BasicGameState {
-	private int state, index = 1;
+	private int state, index = 0;
 	private Rectangle rekt = new Rectangle(0,0,350,80);
     private Image imgWallpaper, imgLogo, imgEngine;
-	private Button btnNewGame, btnLoad, btnExit;
+    private List<Button> buttons = new ArrayList<>();
 	public Menu(int state) { this.state = state; }
 
 	@Override
-	public void init(GameContainer gc, StateBasedGame game)
+	public void init(final GameContainer gc, final StateBasedGame game)
 			throws SlickException {
         ImageManager img = new ImageManager();
         imgLogo = img.getImage(0);
         imgWallpaper = img.getImage(4);
         imgEngine = img.getImage(5);
 
-        btnNewGame = new Button(img.getImage(1), gc.getWidth()/2, 250);
-        btnLoad = new Button(img.getImage(2), gc.getWidth()/2, 350);
-        btnExit = new Button(img.getImage(3), gc.getWidth()/2, 450);
-	}
+        buttons.add(new Button(img.getImage(1), gc.getWidth()/2, 250)); // new game
+        buttons.add(new Button(img.getImage(2), gc.getWidth()/2, 350)); // load
+        buttons.add(new Button(img.getImage(3), gc.getWidth()/2, 450)); // exit
 
-	@Override
-	public void render(final GameContainer gc, final StateBasedGame game, Graphics g)
-			throws SlickException {
-		
-        imgWallpaper.drawCentered(gc.getWidth()/2, 350);
-        imgLogo.drawCentered(gc.getWidth()/2, 100);
-        imgEngine.draw(1000, 450);
-
-        g.setColor(Color.white);
-        g.fill(rekt);
-        btnNewGame.drawCentered();
-        btnLoad.drawCentered();
-        btnExit.drawCentered();
-        btnNewGame.setRunnable(new Runnable() {
+        buttons.get(0).setRunnable(new Runnable() { // new game
             @Override
             public void run() {
                 game.enterState(Main.play);
             }
         });
-        btnExit.setRunnable(new Runnable() {
+        buttons.get(2).setRunnable(new Runnable() { // exit
             @Override
             public void run() {
                 gc.exit();
@@ -57,39 +46,42 @@ public class Menu extends BasicGameState {
 	}
 
 	@Override
+	public void render(GameContainer gc, StateBasedGame game, Graphics g)
+			throws SlickException {
+		
+        imgWallpaper.drawCentered(gc.getWidth()/2, 350);
+        imgLogo.drawCentered(gc.getWidth()/2, 100);
+        imgEngine.draw(1000, 450);
+
+        g.setColor(Color.white);
+        g.fill(rekt);
+        for(Button btn : buttons) {
+            btn.drawCentered();
+        }
+	}
+
+	@Override
 	public void update(GameContainer gc, StateBasedGame game, int delta)
 			throws SlickException {
 		rekt.setCenterX(gc.getWidth()/2);
-		switch (index) {
-			case 1: 
-				rekt.setCenterY(249);
-				break;
-			case 2: 
-				rekt.setCenterY(349);
-				break;
-			case 3:
-				rekt.setCenterY(449);
-				break;
-		}
+        rekt.setCenterY(buttons.get(index).getY());
 	}
 
 	public void keyPressed(int key, char c) {
 		if(key == Input.KEY_UP) index--;
 		if(key == Input.KEY_DOWN) index++;
 
-		if(index < 1) index = 3;
-		if(index > 3) index = 1;
+		if(index < 0) index = buttons.size() - 1;
+		if(index >= buttons.size()) index = 0;
 
 		if (key == Input.KEY_ENTER) runAction();
 	}
 
     public void mouseMoved(int oldx, int oldy, int newx, int newy) {
-        if(btnNewGame.isOver(newx, newy)) {
-            index = 1;
-        } else if(btnLoad.isOver(newx, newy)) {
-            index = 2;
-        } else if(btnExit.isOver(newx, newy)) {
-            index = 3;
+        for(int i = 0; i < buttons.size(); i++) {
+            if(buttons.get(i).isOver(newx, newy)) {
+                index = i;
+            }
         }
     }
 
@@ -97,10 +89,11 @@ public class Menu extends BasicGameState {
         if(button != 0) return;
 
         // The active menu option is fired if the cursor is over one of them
-        if(btnNewGame.isOver(x, y)
-                || btnLoad.isOver(x, y)
-                || btnExit.isOver(x, y)) {
-            runAction();
+        for(Button btn : buttons) {
+            if(btn.isOver(x, y)) {
+                runAction();
+                break;
+            }
         }
     }
 
@@ -108,11 +101,7 @@ public class Menu extends BasicGameState {
      * Runs the actual menu action
      */
     public void runAction() {
-        switch (index) {
-            case 1: btnNewGame.run(); break;
-            case 2: btnLoad.run(); break;
-            case 3: btnExit.run(); break;
-        }
+        buttons.get(index).run();
     }
 
 	@Override
